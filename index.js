9f81fe40d0ce0c5b8528af0519a7cbfdfbe1233d
unknown <limajhlima@gmail.com>
2025-10-17 07:06:20 -0300

    inicando projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+// Seleciona elementos principais
+const taskNameInput = document.getElementById("taskName");
+const taskTagInput = document.getElementById("taskTag");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const taskCont = document.getElementById("taskCount");
+
+
+ 
